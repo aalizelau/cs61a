24c3550a0cae03ce07973f90babe231ef58685ad
aalizelau <aalizelau@gmail.com>
2024-06-15 11:32:19 +0800

    completed

--- hw02.py
+++ hw02.py
@@ -58,7 +58,11 @@ def accumulate(fuse, start, n, term):
     >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
     19
     """
-    "*** YOUR CODE HERE ***"
+    total,k = start,1
+    while k<=n:
+        total = fuse(total,term(k))
+        k = k+1
+    return total 
 
 
 def summation_using_accumulate(n, term):
@@ -73,7 +77,7 @@ def summation_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(add,0,n,term)
 
 
 def product_using_accumulate(n, term):
@@ -88,7 +92,7 @@ def product_using_accumulate(n, term):
     >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
     ['Expr', 'Return']
     """
-    return ____
+    return accumulate(mul,1,n,term)
 
 
 def make_repeater(f, n):
@@ -104,4 +108,10 @@ def make_repeater(f, n):
     >>> make_repeater(square, 3)(5) # square(square(square(5)))
     390625
     """
-    "*** YOUR CODE HERE ***"
+    def repeater(x):
+        k=0 
+        while k<n:
+            x=f(x)
+            k=k+1
+        return x
+    return repeater
