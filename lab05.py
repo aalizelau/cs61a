421767f8be049c3c8a4ac262694db3b1467f727b
aalizelau <aalizelau@gmail.com>
2024-06-28 17:29:37 +0800

    completed

--- lab05.py
+++ lab05.py
@@ -192,7 +192,10 @@ def sprout_leaves(t, leaves):
           1
           2
     """
-    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return tree(label(t), [tree(leaf) for leaf in leaves])
+    else:
+        return tree(label(t), [sprout_leaves(b, leaves) for b in branches(t)])
 
 # Abstraction tests for sprout_leaves and berry_finder
 def check_abstraction():
@@ -241,127 +244,6 @@ def check_abstraction():
           2
     >>> change_abstraction(False)
     """
-
-
-def coords(fn, seq, lower, upper):
-    """
-    >>> seq = [-4, -2, 0, 1, 3]
-    >>> fn = lambda x: x**2
-    >>> coords(fn, seq, 1, 9)
-    [[-2, 4], [1, 1], [3, 9]]
-    """
-    "*** YOUR CODE HERE ***"
-    return ______
-
-
-def riffle(deck):
-    """Produces a single, perfect riffle shuffle of DECK, consisting of
-    DECK[0], DECK[M], DECK[1], DECK[M+1], ... where M is position of the
-    second half of the deck.  Assume that len(DECK) is even.
-    >>> riffle([3, 4, 5, 6])
-    [3, 5, 4, 6]
-    >>> riffle(range(20))
-    [0, 10, 1, 11, 2, 12, 3, 13, 4, 14, 5, 15, 6, 16, 7, 17, 8, 18, 9, 19]
-    """
-    "*** YOUR CODE HERE ***"
-    return _______
-
-
-def add_trees(t1, t2):
-    """
-    >>> numbers = tree(1,
-    ...                [tree(2,
-    ...                      [tree(3),
-    ...                       tree(4)]),
-    ...                 tree(5,
-    ...                      [tree(6,
-    ...                            [tree(7)]),
-    ...                       tree(8)])])
-    >>> print_tree(add_trees(numbers, numbers))
-    2
-      4
-        6
-        8
-      10
-        12
-          14
-        16
-    >>> print_tree(add_trees(tree(2), tree(3, [tree(4), tree(5)])))
-    5
-      4
-      5
-    >>> print_tree(add_trees(tree(2, [tree(3)]), tree(2, [tree(3), tree(4)])))
-    4
-      6
-      4
-    >>> print_tree(add_trees(tree(2, [tree(3, [tree(4), tree(5)])]), \
-    tree(2, [tree(3, [tree(4)]), tree(5)])))
-    4
-      6
-        8
-        5
-      5
-    """
-    "*** YOUR CODE HERE ***"
-
-
-def build_successors_table(tokens):
-    """Return a dictionary: keys are words; values are lists of successors.
-
-    >>> text = ['We', 'came', 'to', 'investigate', ',', 'catch', 'bad', 'guys', 'and', 'to', 'eat', 'pie', '.']
-    >>> table = build_successors_table(text)
-    >>> sorted(table)
-    [',', '.', 'We', 'and', 'bad', 'came', 'catch', 'eat', 'guys', 'investigate', 'pie', 'to']
-    >>> table['to']
-    ['investigate', 'eat']
-    >>> table['pie']
-    ['.']
-    >>> table['.']
-    ['We']
-    """
-    table = {}
-    prev = '.'
-    for word in tokens:
-        if prev not in table:
-            "*** YOUR CODE HERE ***"
-        "*** YOUR CODE HERE ***"
-        prev = word
-    return table
-
-def construct_sent(word, table):
-    """Prints a random sentence starting with word, sampling from
-    table.
-
-    >>> table = {'Wow': ['!'], 'Sentences': ['are'], 'are': ['cool'], 'cool': ['.']}
-    >>> construct_sent('Wow', table)
-    'Wow!'
-    >>> construct_sent('Sentences', table)
-    'Sentences are cool.'
-    """
-    import random
-    result = ''
-    while word not in ['.', '!', '?']:
-        "*** YOUR CODE HERE ***"
-    return result.strip() + word
-
-def shakespeare_tokens(path='shakespeare.txt', url='http://composingprograms.com/shakespeare.txt'):
-    """Return the words of Shakespeare's plays as a list."""
-    import os
-    from urllib.request import urlopen
-    if os.path.exists(path):
-        return open(path, encoding='ascii').read().split()
-    else:
-        shakespeare = urlopen(url)
-        return shakespeare.read().decode(encoding='ascii').split()
-
-# Uncomment the following two lines
-# tokens = shakespeare_tokens()
-# table = build_successors_table(tokens)
-
-def random_sent():
-    import random
-    return construct_sent(random.choice(table['.']), table)
-
 
 
 # Tree ADT
