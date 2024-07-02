81d60fcc7d8abdd4d13964d53720411feccb02f4
aalizelau <aalizelau@gmail.com>
2024-07-02 20:09:22 +0800

    finish Q1

--- hw04.py
+++ hw04.py
@@ -19,7 +19,17 @@ def make_bank(balance):
     120
     """
     def bank(message, amount):
-        "*** YOUR CODE HERE ***"
+        nonlocal balance 
+        if message == 'deposit':
+            balance = balance + amount
+            return balance
+        elif message == 'withdraw':
+            if amount> balance:
+                return 'Insufficient funds'
+            balance = balance - amount
+            return balance
+        else:
+            return 'Invalid message'
     return bank
 
 
