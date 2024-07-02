f27a21e4fabd9bd351f5c8e2a3702d1d0dbddd37
aalizelau <aalizelau@gmail.com>
2024-07-02 20:10:17 +0800

    finish Q3

--- hw04.py
+++ hw04.py
@@ -79,30 +79,6 @@ def make_withdraw(balance, password):
     return safer_bank
 
 
-def repeated(t, k):
-    """Return the first value in iterator T that appears K times in a row. Iterate through the items such that
-    if the same iterator is passed into repeated twice, it continues in the second call at the point it left off
-    in the first.
-
-    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
-    >>> repeated(s, 2)
-    9
-    >>> s2 = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
-    >>> repeated(s2, 3)
-    8
-    >>> s = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
-    >>> repeated(s, 3)
-    2
-    >>> repeated(s, 3)
-    5
-    >>> s2 = iter([4, 1, 6, 6, 7, 7, 8, 8, 2, 2, 2, 5])
-    >>> repeated(s2, 3)
-    2
-    """
-    assert k > 1
-    "*** YOUR CODE HERE ***"
-
-
 def permutations(seq):
     """Generates all permutations of the given sequence. Each permutation is a
     list of the elements in SEQ in a different order. The permutations may be
@@ -125,7 +101,15 @@ def permutations(seq):
     >>> sorted(permutations("ab"))
     [['a', 'b'], ['b', 'a']]
     """
-    "*** YOUR CODE HERE ***"
+    if len(seq) == 0:
+        yield []
+    elif len(seq) == 1:
+        yield [seq[0]]
+    else:
+        for i in range(len(seq)):
+            remaining = seq[:i] + seq[i+1:] 
+            for perm in permutations(remaining):
+                yield [seq[i]] + perm
 
 
 def make_joint(withdraw, old_pass, new_pass):
